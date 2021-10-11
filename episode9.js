bbf3f1fb1d0acf449b6522766f9fbb4b82fdaec3
Karen Stewart <kastewart2002@googlemail.com>
2021-10-11 17:49:32 +0100

    episode 9 acted as suspected

---
+++ episode9.js
@@ -0,0 +1,18 @@
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The murderer is Professor Plum
+
+// because of the introduction of another declaration with a let at line 4, js leaves murderer set to Professor Plum from before
+
+
