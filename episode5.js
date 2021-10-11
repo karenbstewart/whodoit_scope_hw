f82ab8be78a6bb9e530d181054485cbccb3735ab
Karen Stewart <kastewart2002@googlemail.com>
2021-10-11 17:26:38 +0100

    episode 6 acted as suspeced

---
+++ episode5.js
@@ -0,0 +1,21 @@
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
+  
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
+  
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+// WRONG - changeWeapon function can not change Candle Stick to a Revolver. 
+// The weapon is the Revolver.
+
