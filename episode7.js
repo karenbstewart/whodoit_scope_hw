f9f904000ab434c282bea10cdeb4bf127b633b5c
Karen Stewart <kastewart2002@googlemail.com>
2021-10-11 17:45:58 +0100

    episode 8 acted as suspected

---
+++ episode7.js
@@ -0,0 +1,30 @@
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// WRONG - The murderer is Colonel Mustard
+// why is plotTwist not called? added declaration in plotTwist if that is removed it makes -
+//The murderer is Miss Scarlet.
+
