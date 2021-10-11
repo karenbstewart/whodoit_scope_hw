cc8ddf586080485818ca59cd5ca1f22619bb55b3
Karen Stewart <kastewart2002@googlemail.com>
2021-10-11 17:19:23 +0100

    episode 4 acting as suspected

---
+++ episode4.js
@@ -0,0 +1,17 @@
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+
+
+// The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// Suspect three is Mrs. Peacock
